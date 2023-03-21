98494f2e16e6763991994a42da0e31a8e3fbf9f9
GabrielaGeorgievaSQA <gabjab66@gmail.com>
2023-03-21 12:11:28 +0200

    few changes4

--- terraform/7-kubernetes.tf
+++ terraform/7-kubernetes.tf
@@ -11,6 +11,10 @@ resource "google_container_cluster" "primary"{
   monitoring_service       = "monitoring.googleapis.com/kubernetes"
   networking_mode          = "VPC_NATIVE"
 
+  node_locations = [
+    "us-central1-b"
+  ]
+
   addons_config{
     http_load_balancing {
       disabled = true
@@ -20,22 +24,22 @@ resource "google_container_cluster" "primary"{
     }
   } 
 
-  release_channel{
+  release_channel {
     channel  = "REGULAR"
   }
   
-  workload_identity_config{
-    workload_pool = "devops-v4.svc.id.google"
+  workload_identity_config {
+    workload_pool = "devops-v4.svc.id.goog"
   }
 
-  ip_allocation_policy{
+  ip_allocation_policy {
     cluster_secondary_range_name  = "k8s-pod-range"
     services_secondary_range_name = "k8s-service-range"
   }
 
-  private_cluster_config{
-    enable_private_nodes   = true
+  private_cluster_config {
+    enable_private_nodes    = true
     enable_private_endpoint = false
-    master_ipv4_cidr_block = "172.16.0.0/28"
+    master_ipv4_cidr_block  = "172.16.0.0/28"
   }
 }
